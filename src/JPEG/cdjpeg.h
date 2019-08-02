aaf0f3f783386b3c0c31e2401db4a9ea145e1574
anoobzg <anoob@sina.cn>
2019-08-02 22:17:19 +0800

    djpeg

--- src/JPEG/cdjpeg.h
+++ src/JPEG/cdjpeg.h
@@ -16,80 +16,82 @@
 #include "jerror.h"		/* get library error codes too */
 #include "cderror.h"		/* get application-specific error codes */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+	/*
+	 * Object interface for cjpeg's source file decoding modules
+	 */
 
-/*
- * Object interface for cjpeg's source file decoding modules
- */
+	typedef struct cjpeg_source_struct* cjpeg_source_ptr;
 
-typedef struct cjpeg_source_struct * cjpeg_source_ptr;
+	struct cjpeg_source_struct {
+		JMETHOD(void, start_input, (j_compress_ptr cinfo,
+			cjpeg_source_ptr sinfo));
+		JMETHOD(JDIMENSION, get_pixel_rows, (j_compress_ptr cinfo,
+			cjpeg_source_ptr sinfo));
+		JMETHOD(void, finish_input, (j_compress_ptr cinfo,
+			cjpeg_source_ptr sinfo));
 
-struct cjpeg_source_struct {
-  JMETHOD(void, start_input, (j_compress_ptr cinfo,
-			      cjpeg_source_ptr sinfo));
-  JMETHOD(JDIMENSION, get_pixel_rows, (j_compress_ptr cinfo,
-				       cjpeg_source_ptr sinfo));
-  JMETHOD(void, finish_input, (j_compress_ptr cinfo,
-			       cjpeg_source_ptr sinfo));
+		FILE* input_file;
 
-  FILE *input_file;
-
-  JSAMPARRAY buffer;
-  JDIMENSION buffer_height;
-};
-
-
-/*
- * Object interface for djpeg's output file encoding modules
- */
-
-typedef struct djpeg_dest_struct * djpeg_dest_ptr;
-
-struct djpeg_dest_struct {
-  /* start_output is called after jpeg_start_decompress finishes.
-   * The color map will be ready at this time, if one is needed.
-   */
-  JMETHOD(void, start_output, (j_decompress_ptr cinfo,
-			       djpeg_dest_ptr dinfo));
-  /* Emit the specified number of pixel rows from the buffer. */
-  JMETHOD(void, put_pixel_rows, (j_decompress_ptr cinfo,
-				 djpeg_dest_ptr dinfo,
-				 JDIMENSION rows_supplied));
-  /* Finish up at the end of the image. */
-  JMETHOD(void, finish_output, (j_decompress_ptr cinfo,
-				djpeg_dest_ptr dinfo));
-
-  /* Target file spec; filled in by djpeg.c after object is created. */
-  FILE * output_file;
-
-  /* Output pixel-row buffer.  Created by module init or start_output.
-   * Width is cinfo->output_width * cinfo->output_components;
-   * height is buffer_height.
-   */
-  JSAMPARRAY buffer;
-  JDIMENSION buffer_height;
-};
+		JSAMPARRAY buffer;
+		JDIMENSION buffer_height;
+	};
 
 
-/*
- * cjpeg/djpeg may need to perform extra passes to convert to or from
- * the source/destination file format.  The JPEG library does not know
- * about these passes, but we'd like them to be counted by the progress
- * monitor.  We use an expanded progress monitor object to hold the
- * additional pass count.
- */
+	/*
+	 * Object interface for djpeg's output file encoding modules
+	 */
 
-struct cdjpeg_progress_mgr {
-  struct jpeg_progress_mgr pub;	/* fields known to JPEG library */
-  int completed_extra_passes;	/* extra passes completed */
-  int total_extra_passes;	/* total extra */
-  /* last printed percentage stored here to avoid multiple printouts */
-  int percent_done;
-};
+	typedef struct djpeg_dest_struct* djpeg_dest_ptr;
 
-typedef struct cdjpeg_progress_mgr * cd_progress_ptr;
+	struct djpeg_dest_struct {
+		/* start_output is called after jpeg_start_decompress finishes.
+		 * The color map will be ready at this time, if one is needed.
+		 */
+		JMETHOD(void, start_output, (j_decompress_ptr cinfo,
+			djpeg_dest_ptr dinfo));
+		/* Emit the specified number of pixel rows from the buffer. */
+		JMETHOD(void, put_pixel_rows, (j_decompress_ptr cinfo,
+			djpeg_dest_ptr dinfo,
+			JDIMENSION rows_supplied));
+		/* Finish up at the end of the image. */
+		JMETHOD(void, finish_output, (j_decompress_ptr cinfo,
+			djpeg_dest_ptr dinfo));
+
+		/* Target file spec; filled in by djpeg.c after object is created. */
+		FILE* output_file;
+
+		/* Output pixel-row buffer.  Created by module init or start_output.
+		 * Width is cinfo->output_width * cinfo->output_components;
+		 * height is buffer_height.
+		 */
+		JSAMPARRAY buffer;
+		JDIMENSION buffer_height;
+	};
 
 
-/* Short forms of external names for systems with brain-damaged linkers. */
+	/*
+	 * cjpeg/djpeg may need to perform extra passes to convert to or from
+	 * the source/destination file format.  The JPEG library does not know
+	 * about these passes, but we'd like them to be counted by the progress
+	 * monitor.  We use an expanded progress monitor object to hold the
+	 * additional pass count.
+	 */
+
+	struct cdjpeg_progress_mgr {
+		struct jpeg_progress_mgr pub;	/* fields known to JPEG library */
+		int completed_extra_passes;	/* extra passes completed */
+		int total_extra_passes;	/* total extra */
+		/* last printed percentage stored here to avoid multiple printouts */
+		int percent_done;
+	};
+
+	typedef struct cdjpeg_progress_mgr* cd_progress_ptr;
+
+
+	/* Short forms of external names for systems with brain-damaged linkers. */
 
 #ifdef NEED_SHORT_EXTERNAL_NAMES
 #define jinit_read_bmp		jIRdBMP
@@ -117,43 +119,43 @@ typedef struct cdjpeg_progress_mgr * cd_progress_ptr;
 
 /* Module selection routines for I/O modules. */
 
-EXTERN(cjpeg_source_ptr) jinit_read_bmp JPP((j_compress_ptr cinfo));
-EXTERN(djpeg_dest_ptr) jinit_write_bmp JPP((j_decompress_ptr cinfo,
-					    boolean is_os2));
-EXTERN(cjpeg_source_ptr) jinit_read_gif JPP((j_compress_ptr cinfo));
-EXTERN(djpeg_dest_ptr) jinit_write_gif JPP((j_decompress_ptr cinfo));
-EXTERN(cjpeg_source_ptr) jinit_read_ppm JPP((j_compress_ptr cinfo));
-EXTERN(djpeg_dest_ptr) jinit_write_ppm JPP((j_decompress_ptr cinfo));
-EXTERN(cjpeg_source_ptr) jinit_read_rle JPP((j_compress_ptr cinfo));
-EXTERN(djpeg_dest_ptr) jinit_write_rle JPP((j_decompress_ptr cinfo));
-EXTERN(cjpeg_source_ptr) jinit_read_targa JPP((j_compress_ptr cinfo));
-EXTERN(djpeg_dest_ptr) jinit_write_targa JPP((j_decompress_ptr cinfo));
+	EXTERN(cjpeg_source_ptr) jinit_read_bmp JPP((j_compress_ptr cinfo));
+	EXTERN(djpeg_dest_ptr) jinit_write_bmp JPP((j_decompress_ptr cinfo,
+		boolean is_os2));
+	EXTERN(cjpeg_source_ptr) jinit_read_gif JPP((j_compress_ptr cinfo));
+	EXTERN(djpeg_dest_ptr) jinit_write_gif JPP((j_decompress_ptr cinfo));
+	EXTERN(cjpeg_source_ptr) jinit_read_ppm JPP((j_compress_ptr cinfo));
+	EXTERN(djpeg_dest_ptr) jinit_write_ppm JPP((j_decompress_ptr cinfo));
+	EXTERN(cjpeg_source_ptr) jinit_read_rle JPP((j_compress_ptr cinfo));
+	EXTERN(djpeg_dest_ptr) jinit_write_rle JPP((j_decompress_ptr cinfo));
+	EXTERN(cjpeg_source_ptr) jinit_read_targa JPP((j_compress_ptr cinfo));
+	EXTERN(djpeg_dest_ptr) jinit_write_targa JPP((j_decompress_ptr cinfo));
 
-/* cjpeg support routines (in rdswitch.c) */
+	/* cjpeg support routines (in rdswitch.c) */
 
-EXTERN(boolean) read_quant_tables JPP((j_compress_ptr cinfo, char * filename,
-				       boolean force_baseline));
-EXTERN(boolean) read_scan_script JPP((j_compress_ptr cinfo, char * filename));
-EXTERN(boolean) set_quality_ratings JPP((j_compress_ptr cinfo, char *arg,
-					 boolean force_baseline));
-EXTERN(boolean) set_quant_slots JPP((j_compress_ptr cinfo, char *arg));
-EXTERN(boolean) set_sample_factors JPP((j_compress_ptr cinfo, char *arg));
+	EXTERN(boolean) read_quant_tables JPP((j_compress_ptr cinfo, char* filename,
+		boolean force_baseline));
+	EXTERN(boolean) read_scan_script JPP((j_compress_ptr cinfo, char* filename));
+	EXTERN(boolean) set_quality_ratings JPP((j_compress_ptr cinfo, char* arg,
+		boolean force_baseline));
+	EXTERN(boolean) set_quant_slots JPP((j_compress_ptr cinfo, char* arg));
+	EXTERN(boolean) set_sample_factors JPP((j_compress_ptr cinfo, char* arg));
 
-/* djpeg support routines (in rdcolmap.c) */
+	/* djpeg support routines (in rdcolmap.c) */
 
-EXTERN(void) read_color_map JPP((j_decompress_ptr cinfo, FILE * infile));
+	EXTERN(void) read_color_map JPP((j_decompress_ptr cinfo, FILE* infile));
 
-/* common support routines (in cdjpeg.c) */
+	/* common support routines (in cdjpeg.c) */
 
-EXTERN(void) enable_signal_catcher JPP((j_common_ptr cinfo));
-EXTERN(void) start_progress_monitor JPP((j_common_ptr cinfo,
-					 cd_progress_ptr progress));
-EXTERN(void) end_progress_monitor JPP((j_common_ptr cinfo));
-EXTERN(boolean) keymatch JPP((char * arg, const char * keyword, int minchars));
-EXTERN(FILE *) read_stdin JPP((void));
-EXTERN(FILE *) write_stdout JPP((void));
+	EXTERN(void) enable_signal_catcher JPP((j_common_ptr cinfo));
+	EXTERN(void) start_progress_monitor JPP((j_common_ptr cinfo,
+		cd_progress_ptr progress));
+	EXTERN(void) end_progress_monitor JPP((j_common_ptr cinfo));
+	EXTERN(boolean) keymatch JPP((char* arg, const char* keyword, int minchars));
+	EXTERN(FILE*) read_stdin JPP((void));
+	EXTERN(FILE*) write_stdout JPP((void));
 
-/* miscellaneous useful macros */
+	/* miscellaneous useful macros */
 
 #ifdef DONT_USE_B_MODE		/* define mode parameters for fopen() */
 #define READ_BINARY	"r"
@@ -184,4 +186,8 @@ EXTERN(FILE *) write_stdout JPP((void));
 #else
 #define EXIT_WARNING  2
 #endif
+#endif
+
+#ifdef __cplusplus
+}
 #endif
